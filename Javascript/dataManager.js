9fe78c284949a376318b996a0c6259e38a1ce752
Rémi <roeland.remi@yahoo.fr>
2021-07-16 15:01:57 +0200

    Création du code permettant de récupérer les données

--- Javascript/dataManager.js
+++ Javascript/dataManager.js
@@ -1,7 +1,11 @@
-export class DataManager{
+/* RÉCUPÈRE LES DONNÉES */
 
+export class DataManager {
+
+  /**
+   * @type {dataFromJson} les données en provenance du json
+   */
   data;
-
   src;
 
   /**
@@ -11,15 +15,58 @@ export class DataManager{
    *
    * @constructor
    */
-  constructor(source){
+  constructor(source) {
     this.src = source;
   }
 
+  /**
+   * recupère les données 
+   *
+   * @return  {Promise}  [return description]
+   */
   async getData() {
-    const response = await fetch(this.src + "/data.json");
+    const response = await fetch(this.src + "/data.json"); //on aurait pu faire un import direct mais c'est l'occasion de mettre en place un fetch
     this.data = await response.json();
-    console.log(this.data);
-    return this.data;
+    return true;
   }
 
+
+  /**
+   * permet d'obtenir la liste des photographes
+   *
+   * @return  {Array.<photographerFromJson>}  les photographes
+   */
+  getPhotographersList() {
+    return this.data.photographers;
+  }
+
+  /**
+   * permet d'obtenir les informations necessaires pour une page d'un photographe
+   *
+   * @param   {Number}                photographerId  l'identifiant du photographe
+   *
+   * @return  {photographerPageData}                    [return description]
+   */
+  getPhotographer(photographerId) {
+    //TODO : filtrer les données et retourner celles nécessaires
+
+    const answer = {
+      photographer:null,
+      media: []
+    };
+
+    for (let i = 0; i < this.data.photographers.length; i++) {
+      if (this.data.photographers[i].id === photographerId) {
+        answer.photographer = this.data.photographers[i];
+        break;
+      }
+    }
+
+    for (let i = 0; i < this.data.media.length; i++) {
+      if (this.data.media[i].photographerId === photographerId) {
+        answer.media.push(this.data.media[i]);
+      }
+    }
+    return answer;
+  }
 }
