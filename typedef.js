cfba98d7bb1f0eee171143e39e9eb3b15c1e8682
Rémi <roeland.remi@yahoo.fr>
2021-07-16 14:59:31 +0200

    Ajout du fichier typedef.js

---
+++ typedef.js
@@ -0,0 +1,37 @@
+  /**
+   * @typedef  {Object}         photographerFromJson
+   * @property {String}         city
+   * @property {String}         country
+   * @property {Number}         id
+   * @property {String}         name
+   * @property {String}         portrait
+   * @property {Number}         price
+   * @property {String}         tagline
+   * @property {Array.<String>} tags
+   */
+
+  /**
+   * @typedef  {Object}         mediaFromJson
+   * @property {Date}           date
+   * @property {Number}         id
+   * @property {String}         [image]
+   * @property {String}         [video]
+   * @property {Number}         likes
+   * @property {Number}         photographerId
+   * @property {Number}         price
+   * @property {Array.<String>} tags
+   * @property {String}         title
+   * @property {String}         description
+   * /
+
+  /**
+   * @typedef  {Object}                       dataFromJson
+   * @property {Array.<photographerFromJson>} photographers
+   * @property {Array.<mediaFromJson>}        media
+   */
+
+  /**
+   * @typedef  {Object}                photographerPageData
+   * @property {photographerFromJson}  photographer
+   * @property {Array.<mediaFromJson>} media
+   */
