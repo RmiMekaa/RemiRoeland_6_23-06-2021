c613cac81472fc635131c96cd61e552b4b4508e0
Rémi <roeland.remi@yahoo.fr>
2021-07-16 15:10:51 +0200

    Refactorisation

--- Javascript/components/profile.js
+++ Javascript/components/profile.js
@@ -1,72 +1,70 @@
+/* CONSTRUIT LES PROFILS POUR LA PAGE D'ACCUEIL */
+
 export class Profile {
 
-    /**
-    * la représentation du profil dans le DOM
-    * @type {HTMLElement}
-    */
-    profile;
-    /**
-     * le nom du photographe
-     * @type {String}
-     */
-    name;
-    /**
-    * le numéro d'identifiant
-    * @type {Number}
-    */
-    id;
-    /**
-     * la ville du photographe
-     * @type {String}
-     */
-    city;
-    /**
-     * le pays du photographe
-     * @type {String}
-     */
-    country;
-    /**
-     * la liste des tags
-     * @type {Array}
-     */
-    tags;
-    /**
-    * la phrase de description
-    * @type {String}
-    */
-    tagline;
-    /**
-    * le tarif du photographe
-    * @type {Number}
-    */
-    price;
-    /**
-    * la photo de profil du photographe
-    * @type {HTMLElement}
-    */
-    portrait;
+  /**
+  * la représentation du profil dans le DOM
+  * @type {HTMLElement}
+  */
+  profile;
+  /**
+   * le nom du photographe
+   * @type {String}
+   */
+  name;
+  /**
+  * le numéro d'identifiant
+  * @type {Number}
+  */
+  id;
+  /**
+   * la ville du photographe
+   * @type {String}
+   */
+  city;
+  /**
+   * le pays du photographe
+   * @type {String}
+   */
+  country;
+  /**
+   * la liste des tags
+   * @type {Array}
+   */
+  tags;
+  /**
+  * la phrase de description
+  * @type {String}
+  */
+  tagline;
+  /**
+  * le tarif du photographe
+  * @type {Number}
+  */
+  price;
+  /**
+  * la photo de profil du photographe
+  * @type {HTMLElement}
+  */
+  portrait;
 
-    /**
-    * [constructor description]
-    *
-    * @param   {HTMLElement}  domTarget  le noeud où l'on va injecter notre profil
-    * @param   {Object}       infos      les détails du photographe
-    *
-    * @constructor
-    */
-    constructor(domTarget, infos) {
-        this.profile = document.createElement("article");
-        this.profile.className = "photographer";
-        domTarget.appendChild(this.profile);
-        for (const [key, value] of Object.entries(infos)) {
-            this[key] = value;
-        }
-        // this.render();
+  /**
+  * [constructor description]
+  *
+  * @param   {Object}       infos      les détails du photographe
+  *
+  * @constructor
+  */
+  constructor(infos) {
+    for (const [key, value] of Object.entries(infos)) {
+      this[key] = value;
     }
+  }
 
-    render() {
-        return `
-        <a class="photographer__thumb" href="photographer-page.html" aria-label:"${this.name}">
+  html() {
+    return `
+        <article class="photographer">
+        <a class="photographer__thumb" href="?photographer/${this.id}" aria-label:"${this.name}">
             <img class="photographer__pp" src="ressources/Sample Photos/Photographers ID Photos/thumbs/${this.portrait}" alt="${this.name}">
             <h2 class="photographer__name">${this.name}</h2>
         </a>
@@ -76,15 +74,18 @@ export class Profile {
         <div class="photographer__tags">
             ${this.tagList()}
         </div>
+        </article>
         `;
-    }
+  }
 
-    tagList() {
-        let list = "";
-        for (let i = 0; i < this.tags.length; i++) {
-            list += "<a class=\"tag\" href=\"\">#" + this.tags[i] + "</a>";
-        }
-        return list;
+  tagList() {
+    let list = "";
+    for (let i = 0; i < this.tags.length; i++) {
+      console.log("this.tags[i]:", this.tags[i]);
+
+      list += `<a class="tag" href="#${this.tags[i]}">#${this.tags[i]}</a>`;
     }
+    return list;
+  }
 }
 
