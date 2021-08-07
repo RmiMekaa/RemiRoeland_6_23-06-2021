1171c22e4ba0aac468ef661e7f7d3580a1707bd6
Rémi <roeland.remi@yahoo.fr>
2021-08-07 14:28:27 +0200

    Réparation du slider

--- Javascript/pages/showMedia.js
+++ Javascript/pages/showMedia.js
@@ -1,15 +1,16 @@
 /* SLIDER POUR LES MÉDIAS */
 
 import { Media } from "../components/media.js";
-import { PhotographerPage } from "./photographerPage.js";
 
 export class ShowMedia {
 
   constructor(photographerId, medias, actualMedia) {
     this.photographerId = photographerId; // L'id du photographe
     this.medias = medias; // Un tableau contenant des objets (les médias)
+    this.actualMedia = actualMedia;
+    console.log(this.medias);
     console.log(actualMedia);
-    this.currentItem = actualMedia;    
+    this.currentItem = this.medias.findIndex(i => (i.image || i.video) === this.actualMedia);
   }
 
   /**
@@ -30,11 +31,11 @@ export class ShowMedia {
   createMedias() {
     this.media = {};
     let html = '<div id="medias">';
-    for (let i = 0; i < this.medias.length; i++) {
-      this.media["media" + this.medias[i].id] = new Media(this.medias[i]);
-      html += this.media["media" + this.medias[i].id].html();
-    }
-    return html += '</div>';
+    // for (let i = 0; i < this.medias.length; i++) {
+      const media = new Media(this.medias[this.currentItem]);
+      html += media.html();
+    // }
+    return html + '</div>';
   }
 
   /**
@@ -42,51 +43,44 @@ export class ShowMedia {
    * @return  {String}  HTML String
    */
   createNavigation() {
-    return `<button class="slider__next" onclick="page.next()"></button>
-            <button class="slider__prev" onclick="page.prev()"></button>
-            <button class="slider__close" onclick="window.history.go(-1); return false;"></button>
+    return `<a href="?showmedia/${this.nextMedia}"><button class="slider__next"></button></a>
+            <a href="?showmedia/${this.prevMedia}"><button class="slider__prev"></button></a>
+            <a href="?photographer/${this.photographerId}"><button class="slider__close"; return false;"></button></a>
             `;
   }
 
-  next() {
-    const items = document.querySelectorAll("#medias .item");
-    console.log(this.medias);
-    const nbSlide = items.length;
 
-    items[this.currentItem].classList.remove('active');
 
-    if (this.currentItem < nbSlide - 1) {
-      this.currentItem++;
-    } else {
-      this.currentItem = 0;
-    }
 
-    items[this.currentItem].classList.add('active');
-    console.log(this.currentItem);
+  /*----- FONCTIONNALITÉS POUR LA NAVIGATION -----*/
+  
+  get nextMedia() {
+    let nextMediaId= this.currentItem+1;
+    if (nextMediaId >= this.medias.length) nextMediaId = 0;
+    if (this.medias[nextMediaId].hasOwnProperty('image')) return this.photographerId+"/"+ this.medias[nextMediaId].image;
+    else return this.photographerId+"/"+ this.medias[nextMediaId].video;
+  }
+  get prevMedia() {
+    let prevMediaId= this.currentItem-1;
+    if (prevMediaId <0) prevMediaId = this.medias.length -1;
+    if (this.medias[prevMediaId].hasOwnProperty('image')) return this.photographerId+"/"+ this.medias[prevMediaId].image;
+    else return this.photographerId+"/"+ this.medias[prevMediaId].video;
   }
 
-  prev() {
-    const items = document.querySelectorAll("#medias .item");
-    console.log(items);
-    const nbSlide = items.length;
-
-    items[this.currentItem].classList.remove('active');
-
-    if (this.currentItem > 0) {
-      this.currentItem--;
-    } else {
-      this.currentItem = nbSlide -1;
-    }
-
-    items[this.currentItem].classList.add('active');
-    console.log(this.currentItem);
-  }
-
-  //next() {
-  //  this.currentItem++
-  //  let i = this.currentItem;
-  //  this.medias[i].active = true;
-  //  delete this.medias[i-1].active;
+  //prev() {
+  //  const items = document.querySelectorAll("#medias .item");
+  //  const nbSlide = items.length;
+//
+  //  items[this.currentItem].classList.remove('active');
+//
+  //  if (this.currentItem > 0) {
+  //    this.currentItem--;
+  //  } else {
+  //    this.currentItem = nbSlide -1;
+  //  }
+//
+  //  items[this.currentItem].classList.add('active');
+  //  console.log(this.currentItem);
   //}
 
 }
