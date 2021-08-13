ff21092438bbd031874e84a58694087c1f0b1ffb
Rémi <roeland.remi@yahoo.fr>
2021-08-13 16:32:46 +0200

    Ajout d'une méthode pour enfermer le focus dans le formulaire quand celui-ci est ouvert

--- Javascript/pages/photographerPage.js
+++ Javascript/pages/photographerPage.js
@@ -13,6 +13,7 @@ export class PhotographerPage {
   constructor(data) {
     this.data = data;
     this.activeTags = [];
+    this.focusList = {};
   }
 
   /**
@@ -21,7 +22,7 @@ export class PhotographerPage {
   * @return  {String}  le HTML de la page
   */
   html() {
-    let html = this.createHeader() + '<main>' + this.createPhotographerResume() + this.createSortBy() + '<div class="gallery" id="gallery">' + this.createMedias() + '</div>' + this.createLikesCounter() + this.createForm() + '</main>';
+    let html = this.createHeader() + '<main>' + this.createPhotographerResume() + this.createSortBy() + '<div class="gallery" id="gallery">' + this.createMedias() + '</div>' + this.createForm() + this.createLikesCounter() + '</main>';
     return html;
   }
 
@@ -59,24 +60,9 @@ export class PhotographerPage {
     }
     return html;
   }
-  /**
-  * Retourne le html du dropdown
-  *
-  * @return  {String}  HTML String
-  */
-  createSortBy(){
-    return `
-    <span class="select-label">Trier par</span>
-    <div class="select" tabindex="1">
-      <input class="sortBy-input" name="option" type="radio" id="popularity" onclick="page.sortBy(this)" checked>
-      <label for="popularity" class="select__option">Popularité</label>
-      <input class="sortBy-input" name="option" type="radio" id="date" onclick="page.sortBy(this)">
-      <label for="date" class="select__option">Date</label>
-      <input class="sortBy-input" name="option" type="radio" id="name" onclick="page.sortBy(this)">
-      <label for="name" class="select__option">Nom</label>
-    </div>
-    `;
-  }
+
+  /*----- LIKES -----*/
+
   /**
   * Retourne le html du compteur de likes
   *
@@ -84,33 +70,10 @@ export class PhotographerPage {
   */
   createLikesCounter() {
     return `<aside class="total-likes">
-                <span id="totalLikesNbr" class="totalLikesNbr">${this.getTotal()}</span>
+                <span id="totalLikesNbr" class="totalLikesNbr" tabindex="0">${this.getTotal()}</span>
                 <span class="price">${this.data.photographer.price}€ / jour</span>
             </aside>`;
   }
-  /**
-  * Retourne le html du formulaire de contact
-  *
-  * @return  {String}  HTML String
-  */
-  createForm() {
-    return `<form id="contactForm">
-              <h1>Contactez-moi </br><span>${this.data.photographer.name}</span></h1>
-              <label for="firstname">Prénom</label>
-              <input type="text" name="firstname" aria-label="Champ du prénom">
-              <label for="lastname">Nom</label>
-              <input type="text" name="lastname" aria-label="Champ du nom">
-              <label for="email">Email</label>
-              <input type="mail" name="email" aria-label="Champ de l'e-mail">
-              <label for="messagee">Votre message</label>
-              <textarea name="message" aria-label="Champ du message"></textarea>
-              <button class="submit-btn" type="submit">Envoyer</button>
-              <button id="modalClose" onclick="page.closeForm()" role="button" aria-label="Fermer le formulaire"><img src="ressources/close-icon.png"></button>
-            </form>`;
-  }
-
-  /*----- LIKES -----*/
-
   /**
    * Incrémente ou décrémente le compteur de like du média
    * @param   {HTMLElement}  element  l'icône like du média
@@ -149,6 +112,26 @@ export class PhotographerPage {
   /*----- FORMULAIRE -----*/
 
   /**
+  * Retourne le html du formulaire de contact
+  *
+  * @return  {String}  HTML String
+  */
+  createForm() {
+    return `<form id="contactForm">
+              <h1>Contactez-moi </br><span>${this.data.photographer.name}</span></h1>
+              <label for="firstname">Prénom</label>
+              <input type="text" name="firstname" aria-label="Champ du prénom" id="firstname">
+              <label for="lastname">Nom</label>
+              <input type="text" name="lastname" aria-label="Champ du nom" id="lastname">
+              <label for="email">Email</label>
+              <input type="mail" name="email" aria-label="Champ de l'e-mail" id="email">
+              <label for="messagee">Votre message</label>
+              <textarea name="message" aria-label="Champ du message" id="message"></textarea>
+              <button class="submit-btn" type="submit" id="submit">Envoyer</button>
+              <button id="modalClose" onclick="page.closeForm()" role="button" aria-label="Fermer le formulaire"><img src="ressources/close-icon.png"></button>
+            </form>`;
+  }
+  /**
  * Ouvre le formulaire
  *
  * @return  {Void}
@@ -158,10 +141,12 @@ export class PhotographerPage {
     let contactForm = document.getElementById("contactForm");
     contactForm.style.display = "flex";
     //Création du background ↓
-    let modalBg = document.createElement("div");
-    modalBg.classList.add("modal-bg");
-    modalBg.style.position = "fixed";
-    document.body.insertBefore(modalBg, document.body.firstChild);
+    let background = document.createElement("div");
+    background.classList.add("modal-bg");
+    background.setAttribute('id', 'modal-bg');
+    background.style.position = "fixed";
+    document.body.insertBefore(background, document.body.firstChild);
+    this.trapFocus();
   }
   /**
   * Ferme le formulaire
@@ -172,10 +157,30 @@ export class PhotographerPage {
     let contactForm = document.getElementById("contactForm");
     contactForm.style.display = "none";
     //Suppression du background ↓
-    let element = document.getElementsByClassName("modal-bg");
-    document.body.removeChild(element[0]);  
+    let background = document.getElementById("modal-bg");
+    background.remove();  
   }
+  /**
+   * Enferme le focus à l'intérieur du formulaire de contact
+   *
+   * @return  {void} 
+   */
+   trapFocus(){
+    let firstname = document.getElementById('firstname');
+    let close = document.getElementById('modalClose');
 
+    firstname.focus();
+    close.addEventListener('focusout', function() {
+      firstname.focus();
+    })
+
+    window.addEventListener('keyup', (e) => {
+      if (e.keyCode === 27) {
+        close.click();
+      }  
+    })
+  }
+  
   /*----- Tags -----*/
 
   /**
@@ -215,6 +220,24 @@ export class PhotographerPage {
   
   /*----- Tri des médias -----*/
 
+  /**
+  * Retourne le html du dropdown
+  *
+  * @return  {String}  HTML String
+  */
+  createSortBy(){
+    return `
+    <span class="select-label">Trier par</span>
+    <div class="select" tabindex="1">
+      <input class="sortBy-input" name="option" type="radio" id="popularity" onclick="page.sortBy(this)" checked>
+      <label for="popularity" class="select__option">Popularité</label>
+      <input class="sortBy-input" name="option" type="radio" id="date" onclick="page.sortBy(this)">
+      <label for="date" class="select__option">Date</label>
+      <input class="sortBy-input" name="option" type="radio" id="name" onclick="page.sortBy(this)">
+      <label for="name" class="select__option">Nom</label>
+    </div>
+    `;
+  }
   /**
    * Tri les médias
    *
