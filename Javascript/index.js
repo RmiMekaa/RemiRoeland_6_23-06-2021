df4d0d08ed5a95621c6f1e5c4a95dbc8d1fd1b08
Rémi <roeland.remi@yahoo.fr>
2021-08-13 16:34:13 +0200

    Nettoyage

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,26 +1,3 @@
 import { PageManager } from "./pageManager.js";
 
 window.pageManager = new PageManager("https://rmimekaa.github.io/RemiRoeland_6_23-06-2021/");
-
-// {/* <input onblur ="page.blurLsitener("nom")> */}
-
-// focusList = {};
-
-// blurLsitener(el){
-//     focusList[el] = false;
-//     setTimeout(500, giveFocus)
-
-// }
-
-// focusListener(el){
-//     focusList[el] = true;
-//     setTimeout(500, giveFocus);
-// }
-
-
-// giveFocus(){
-//     for(const value of Object.values(focusList)){
-//         if (value) return;
-//     }
-//     //on ajoute le focus sur l'élément chjoisi
-// }
