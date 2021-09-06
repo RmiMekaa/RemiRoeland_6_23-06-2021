c6edcf516576f427b22e34ca9250d7e99fbcc871
Rémi <roeland.remi@yahoo.fr>
2021-09-06 15:06:54 +0200

    Création composant header

---
+++ Javascript/components/header.js
@@ -0,0 +1,38 @@
+export class Header{
+
+  /**
+   * Créé une nouvelle instance pour le header
+   *
+   * @param   {( "photographerPage" | "homePage")}  page  la page de référence pour l'affichage du header
+   *
+   * @constructor
+   */
+  constructor(page){
+    this.DOM = document.createElement("header");
+    this.DOM.className ="header";
+    this.DOM.setAttribute('id', 'header');
+    this.DOM.innerHTML = this[page];
+    document.body.appendChild(this.DOM);
+  }
+
+  get photographerPage(){
+    return '<a href="index.html"><img class="header__logo" src="ressources/logo.png" alt="FishEye Home page"></a>';
+  }
+
+  get homePage(){
+    return `<a href="index.html"><img class="header__logo" src="ressources/logo.png" alt="FishEye Home page"></a>
+            <nav class="header__nav">
+                <ul>
+                    <li class="tag" tabindex="0" onclick="page.tagsOnClick(this)">#<span>portrait</span></li>
+                    <li class="tag" tabindex="0" onclick="page.tagsOnClick(this)">#<span>art</span></li>
+                    <li class="tag" tabindex="0" onclick="page.tagsOnClick(this)">#<span>fashion</span></li>
+                    <li class="tag" tabindex="0" onclick="page.tagsOnClick(this)">#<span>architecture</span></li>
+                    <li class="tag" tabindex="0" onclick="page.tagsOnClick(this)">#<span>travel</span></li>
+                    <li class="tag" tabindex="0" onclick="page.tagsOnClick(this)">#<span>sport</span></li>
+                    <li class="tag" tabindex="0" onclick="page.tagsOnClick(this)">#<span>animals</span></li>
+                    <li class="tag" tabindex="0" onclick="page.tagsOnClick(this)">#<span>events</span></li>
+                </ul>
+            </nav>
+            <h1 class="header__heading">Nos photographes</h1>`;
+  }
+}
